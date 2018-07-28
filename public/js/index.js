c8f3c3fc0c7363dcd45e78b9487c24a3b69125d8
Simon Stubben <simonstubben@gmail.com>
2018-07-28 18:15:29 +0200

    Add newMessage and createMessage events

---
+++ public/js/index.js
@@ -0,0 +1,19 @@
+//initiating the request from the client to the server and keep it open
+var socket = io();
+// listen on connect event
+socket.on('connect', function() {
+  console.log('Connected to server')
+
+  socket.emit('createMessage', {
+    from: 'johndoe',
+    text: 'I created a new message'
+  })
+})
+
+socket.on('disconnect', function() {
+  console.log('Disconnected from server')
+})
+
+socket.on('newMessage', function (message) {
+  console.log('newMessage', message);
+})
--- server/server.js
+++ server/server.js
@@ -14,11 +14,36 @@ app.use(express.static('public'))
 // io.on('connection') let's you listen for a new connection
 // meaning a client connected to the server
 io.on('connection', (socket) => {
-  console.log('New user connected');
-  socket.on('disconnect', () => {
-    console.log('Disconnected from client');
+  console.log('New user connected')
+
+  // socket has a method emit that will be used both in client and server
+  // emit is really similar to listeners (on), although instead of listening
+  // to events, emit creates events.
+  // socket.emit('newEmail', {
+  //   from:'hello@doe.com',
+  //   text: 'This is a test',
+  //   createdAt: '12345'
+  // })
+  //
+  // socket.on('createEmail', (newEmail) => {
+  //   console.log('Create email', newEmail)
+  // })
+
+  socket.emit('newMessage', {
+    from: 'johndoe',
+    text: 'Sending you a message!',
+    createdAt: 987654321
   })
-});
+
+  socket.on('createMessage', (message) => {
+    console.log('createMessage', message);
+  })
+
+  socket.on('disconnect', () => {
+    console.log('Disconnected from client')
+  })
+
+})
 
 server.listen(port, () => {
   console.log(`Started on port ${port}`);
